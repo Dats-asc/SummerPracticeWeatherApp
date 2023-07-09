745edc6684a02b057db6c5b98558d6d63b703750
Niyaz <niazr90@mail.ru>
2023-07-09 22:35:16 +0300

    detail screen 4

--- app/src/main/java/com/example/summerpracticeweatherapp/TodayForecastFragment.kt
+++ app/src/main/java/com/example/summerpracticeweatherapp/TodayForecastFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.example.summerpracticeweatherapp.databinding.FragmentSearchBinding
 import com.example.summerpracticeweatherapp.databinding.FragmentTodayForecastBinding
+import com.example.summerpracticeweatherapp.network.Forecast
 
 
 class TodayForecastFragment : Fragment(R.layout.fragment_today_forecast) {
@@ -16,6 +18,16 @@ class TodayForecastFragment : Fragment(R.layout.fragment_today_forecast) {
         super.onViewCreated(view, savedInstanceState)
 
         binding = FragmentTodayForecastBinding.bind(view)
+        binding?.let {
+            with(it){
+                textView3.text = Forecast.forecast?.city?.name
+                Glide.with(requireContext())
+                    .load("http://openweathermap.org/img/w/10d.png")
+                    .into(imageView3)
+            }
+
+        }
+
     }
 
     override fun onDestroyView() {
