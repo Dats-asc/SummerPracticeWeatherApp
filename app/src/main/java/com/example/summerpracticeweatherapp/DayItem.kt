1aaf9a8e39f28f2ebc7c3b1ea7f43ccbf53903c9
Anton <cold-batt1486@mail.ru>
2023-07-09 21:15:06 +0300

    [Update] 10Days fragment

--- app/src/main/java/com/example/summerpracticeweatherapp/DayItem.kt
+++ app/src/main/java/com/example/summerpracticeweatherapp/DayItem.kt
@@ -1,14 +1,17 @@
 package com.example.summerpracticeweatherapp
 import androidx.recyclerview.widget.RecyclerView
 import com.example.summerpracticeweatherapp.databinding.ItemDayBinding
+import com.example.summerpracticeweatherapp.network.Forecast
 
 
 class DayItem(private val binding: ItemDayBinding
 ) : RecyclerView.ViewHolder(binding.root) {
     fun onBind(day: Day){
         binding.run{
-            tvTitle.text = day.name
-            tvDesc.text = day.status.toString()
+            tvData.text = day.date
+            tvDescription.text = day.description
+            tvMaxTemp.text = day.maxTemp.toString()
+            tvMinTemp.text = day.minTemp.toString()
         }
     }
 }
--- app/src/main/java/com/example/summerpracticeweatherapp/DayRepository.kt
+++ app/src/main/java/com/example/summerpracticeweatherapp/DayRepository.kt
@@ -1,22 +1,12 @@
 package com.example.summerpracticeweatherapp
+import com.example.summerpracticeweatherapp.network.Forecast
 import java.text.SimpleDateFormat
 import java.time.LocalDateTime
 import java.util.Calendar
 
 object DayRepository {
-    private val calendar = Calendar.getInstance()
 
-    val list: List<Day> = (0..14).map { index ->
-        val date = calendar.time
-            .toString()
-            .split(" ")
-            .take(3)
-            .joinToString(" ")
-
-        calendar.add(Calendar.DAY_OF_YEAR, 1) //нет сегодняшнего дня, начало со след.дня, надо фиксить
-        Day(status = "*statusWeather*", name = date)
-    }
-
-
-
+    val list: List<Day> = listOf(
+        // Заполнить экземплярами дней
+    )
 }
