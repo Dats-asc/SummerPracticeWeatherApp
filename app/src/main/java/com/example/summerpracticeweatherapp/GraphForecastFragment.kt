3718f6d0f658f8fc309a77c794ada65dc8234c2e
Azat <azv7_78@mail.ru>
2023-07-10 20:55:36 +0300

    Финальная версия

--- app/src/main/java/com/example/summerpracticeweatherapp/GraphForecastFragment.kt
+++ app/src/main/java/com/example/summerpracticeweatherapp/GraphForecastFragment.kt
@@ -21,6 +21,7 @@ import com.jjoe64.graphview.series.DataPoint
 import com.jjoe64.graphview.series.DataPointInterface
 import com.jjoe64.graphview.series.LineGraphSeries
 import com.jjoe64.graphview.series.OnDataPointTapListener
+import com.jjoe64.graphview.series.PointsGraphSeries
 import com.jjoe64.graphview.series.Series
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -39,33 +40,50 @@ class GraphForecastFragment : Fragment(R.layout.fragment_graph_forecast) {
         binding?.tvCity?.text = weather.city.name
         val graphView = binding?.graph
 
-
         val dataPointsMax = DayRepository.maxTempArray.mapIndexed { index, temperature ->
-            DataPoint(index.toDouble(), temperature)
+            DataPoint(index.toDouble() + 1, temperature)
         }.toTypedArray()
         val dataPointsMin = DayRepository.minTempArray.mapIndexed { index, temperature ->
-            DataPoint(index.toDouble(), temperature)
+            DataPoint(index.toDouble() + 1, temperature)
         }.toTypedArray()
 
         val seriesMax = LineGraphSeries(dataPointsMax)
         seriesMax.color = Color.RED
+        val seriesMaxDots = PointsGraphSeries(dataPointsMax)
+        seriesMaxDots.color = Color.RED
+
         val seriesMin = LineGraphSeries(dataPointsMin)
         seriesMin.color = Color.BLUE
+        val seriesMinDots = PointsGraphSeries(dataPointsMin)
+        seriesMinDots.color = Color.BLUE
+
+        val dataPoints = listOf(
+            DataPoint(1.0, 0.0),
+            DataPoint(5.0, 0.0),
+        ).toTypedArray()
+        val seriesDots = PointsGraphSeries(dataPoints)
+        seriesDots.size = 0f
+
+        graphView?.viewport?.setMinX(1.0)
 
         graphView?.addSeries(seriesMax)
         graphView?.addSeries(seriesMin)
+        graphView?.addSeries(seriesDots)
+        graphView?.addSeries(seriesMaxDots)
+        graphView?.addSeries(seriesMinDots)
 
         val tapListener = object : OnDataPointTapListener {
             override fun onTap(series: Series<DataPointInterface>, dataPoint: DataPointInterface) {
                 val temperature = dataPoint.y.toInt().toString()
-                val day = dataPoint.x.toInt() + 1
-                val message = "Temperature on day $day: $temperature°C"
+                val message = "Temperature: $temperature°C"
                 Snackbar.make(binding?.root as View, message, Snackbar.LENGTH_SHORT).show()
             }
         }
 
         seriesMax.setOnDataPointTapListener(tapListener)
         seriesMin.setOnDataPointTapListener(tapListener)
+        seriesMaxDots.setOnDataPointTapListener(tapListener)
+        seriesMinDots.setOnDataPointTapListener(tapListener)
 
     }
 
