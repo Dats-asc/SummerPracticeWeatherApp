e22df07969b6ed1b050c5a3e1f24c63b8710b05d
Niyaz <niazr90@mail.ru>
2023-07-10 15:03:15 +0300

    [Update] today forcekast fragment

--- app/src/main/java/com/example/summerpracticeweatherapp/TodayForecastFragment.kt
+++ app/src/main/java/com/example/summerpracticeweatherapp/TodayForecastFragment.kt
@@ -9,6 +9,7 @@ import com.bumptech.glide.Glide
 import com.example.summerpracticeweatherapp.databinding.FragmentSearchBinding
 import com.example.summerpracticeweatherapp.databinding.FragmentTodayForecastBinding
 import com.example.summerpracticeweatherapp.network.Forecast
+import com.example.summerpracticeweatherapp.utils.loadImage
 
 
 class TodayForecastFragment : Fragment(R.layout.fragment_today_forecast) {
@@ -19,17 +20,26 @@ class TodayForecastFragment : Fragment(R.layout.fragment_today_forecast) {
 
         binding = FragmentTodayForecastBinding.bind(view)
         binding?.let {
+            val forecast =  Forecast.forecast!!
             with(it){
-                textView3.text = Forecast.forecast?.city?.name
-                Glide.with(requireContext())
-                    .load("http://openweathermap.org/img/w/10d.png")
-                    .into(imageView3)
+                tvCity1.text = Forecast.forecast?.city?.name
+                //imageView.loadImage("https://openweathermap.org/img/w/10d.png")
+                ivMainIconNow.loadImage("http://openweathermap.org/img/w/${forecast.list[0].weather[0].icon}.png")
+                ivWeatherNow.loadImage("http://openweathermap.org/img/w/${forecast.list[0].weather[0].icon}.png")
+                tvMainTempValueNow.text = "${forecast.list[0].main.temp.toString()}°"
+                tvInfDayAndNight.text = "Day ${forecast.list[0].main.tempMax.toString()}° " +
+                        "Night ${forecast.list[0].main.tempMin.toString()}°"
+                tvDataAndTime.text = forecast.list[0].dtTxt
+
             }
 
         }
 
     }
-
+//    fun dataTime(){
+//        val data = Forecast.forecast!!.list[0].dtTxt
+//        when()
+//    }
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
